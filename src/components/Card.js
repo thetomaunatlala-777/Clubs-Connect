6e9d15e87dc9b62ad34ad290ff0c97fb2f5cd428
Mmakwena <moichelammakwena7@gmail.com>
2025-08-14 13:11:01 +0200

    added home page, only have a few changes left

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/Card.css';
+import props from 'prop-types';
+
+
+export default function Card(props) {
+  return (
+    <section className="card">
+      <section className="card-content">
+        <img src={props.image} alt="image describing content under" className="card-image" />
+        <p className="card-description">{props.description}</p>
+      </section>
+    </section>
+  );
+}
